8f814e62e868fea20de56db85367ab3875dec322
abhi <vedavally_abhilash@cat.com>
2019-06-01 21:01:30 -0400

    Initial Project template setup

---
+++ src/main/java/com/nalla/pdfstore/mypdfstore/MypdfstoreApplication.java
@@ -0,0 +1,13 @@
+package com.nalla.pdfstore.mypdfstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypdfstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypdfstoreApplication.class, args);
+	}
+
+}
